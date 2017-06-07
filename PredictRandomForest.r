af059a43eda1d5a8964d1686f2d091c74071438a
Azharuddin Kazi <kaziazhar04@gmail.com>
2017-06-07 16:01:22 +0530

    Update PredictRandomForest.r

--- PredictRandomForest.r
+++ PredictRandomForest.r
@@ -1,12 +1,13 @@
 rm(list=ls(all=T))
 
-setwd("D:/R projects/Predict the road sign")
+setwd("SET YOUR WORKING DIRECTORY")
 
 require(randomForest)
 
 train <- read.csv("train.csv", header = TRUE, sep = ",")
 test <- read.csv("test.csv", header = TRUE, sep = ",")
 
+## AS WE HAVE ASPECT RATIO WHICH IS THE RATIO BETWEEN THE WIDTH AND HEIGHT, WE DONT NEED THE EXPLICIT WIDTH AND HEIGHT
 train$SignWidth <- NULL
 train$SignHeight <- NULL
 train$Id <- NULL
@@ -16,6 +17,7 @@ test$SignHeight <- NULL
 testid <- test$Id
 test$Id <- NULL
 
+## RENAMING THE TARGET COLUMN TO SOMETHING NICE
 colnames(train)[4] <- "Target"
 
 model_rf <- randomForest(Target ~ ., train, ntree = 30, mtry = 2, probability = TRUE)
@@ -24,5 +26,6 @@ model_rf
 pred <- data.frame(predict(model_rf, test, type = "prob"))
 final_pred <-cbind(testid,pred)
 
+## MAKING THE DATAFRAME IN REQUIRED FORMAT
 names(final_pred) <- c("Id","Front","Left","Rear","Right")
 write.csv(final_pred,"final_prediction.csv", row.names = FALSE)
